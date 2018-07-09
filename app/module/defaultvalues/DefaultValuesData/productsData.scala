5299167bd15e18eb0fb7d39ae68338a7a4418f3d
Faiz <891366402@qq.com>
2018-07-09 20:44:43 +0800

    哎

--- app/module/defaultvalues/DefaultValuesData/productsData.scala
+++ app/module/defaultvalues/DefaultValuesData/productsData.scala
@@ -12,6 +12,7 @@ trait productsData {
             "prod_code" -> toJson(obj.getAs[String]("prod_code").get),
             "prod_brand" -> toJson(obj.getAs[String]("prod_brand").get),
             "prod_name" -> toJson(obj.getAs[String]("prod_name").get),
+	        "therapeutic_area" -> toJson(obj.getAs[String]("therapeutic_area").get),
             "prod_sales_time" -> toJson(obj.getAs[String]("prod_sales_time").get),
             "prod_unit_price" -> toJson(obj.getAs[String]("prod_unit_price").get),
             "prod_unit_cost" -> toJson(obj.getAs[String]("prod_unit_cost").get),
